f93cf9c44e65634da0fa26e46a232617ff784e40
mahisurrahman <mahisur.rahman.001@gmail.com>
2024-02-23 00:45:03 +0600

    Added the Customization of Navbar

--- Front End/src/Components/Navbar/Navbar.jsx
+++ Front End/src/Components/Navbar/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
     </div>
   );
   return (
-    <div className="navbar bg-transparent px-10">
+    <div className="navbar px-10 fixed z-10 bg-opacity-50 backdrop-blur-sm bg-black">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
