6e4292cd9ce960be93b33cab17380b9aeb6b6c8e
mahisurrahman <mahisur.rahman.001@gmail.com>
2024-02-26 06:33:16 +0600

    Shifting to PC

--- Front End/src/Pages/Signup/Signup.jsx
+++ Front End/src/Pages/Signup/Signup.jsx
@@ -1,13 +1,21 @@
 import { FaPizzaSlice } from "react-icons/fa6";
 import { Link } from "react-router-dom";
-import './SignUp.css';
+import "./SignUp.css";
+import { useForm } from "react-hook-form";
 
 const Signup = () => {
-   return (
-      <div className="bg-lime-500 w-full h-screen flex items-center font-avent-pro">
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+
+  return (
+    <div className="bg-lime-500 w-full h-screen flex items-center font-avent-pro">
       <div className="w-10/12 mx-auto grid grid-cols-2 items-center justify-center gap-10">
         <div className="h-[500px]">
-          <form className="border-4 px-10 py-5 rounded-2xl border-zinc-950 h-[500px]">
+          <form onSubmit={handleSubmit()} className="border-4 px-10 py-5 rounded-2xl border-zinc-950 h-[500px]">
             <h1 className="text-3xl text-center text-zinc-950 font-extrabold uppercase underline">
               Sign Up with Details
             </h1>
@@ -17,32 +25,40 @@ const Signup = () => {
               name="photo"
               id=""
               placeholder="Photo URL"
+              {...register('photo', {required: true})} 
             />
             <br />
+            {errors.photo && <p>Photo is required.</p>}
             <input
               className="text-md font-bold mt-4 w-full py-2 rounded-lg px-5 outline-none"
               type="text"
               name="name"
               id=""
               placeholder="name"
+              {...register('name', {required: true})} 
             />
             <br />
+            {errors.name && <p>Name is required.</p>}
             <input
               className="text-md font-bold mt-4 w-full py-2 rounded-lg px-5 outline-none"
               type="email"
               name="email"
               id=""
               placeholder="email"
+              {...register('email', {required: true})} 
             />
             <br />
+            {errors.email && <p>Email is required.</p>}
             <input
               className="text-md font-bold mt-4 w-full py-2 rounded-lg px-5 outline-none"
               type="password"
               name="password"
               id=""
               placeholder="password"
+              {...register('password', {required: true})} 
             />
             <br />
+            {errors.password && <p>Password is required.</p>}
             <div className="w-full flex items-center justify-center">
               <input
                 className="duration-700 text-xl font-extrabold text-zinc-950 px-10 py-1 border-2 border-zinc-950 rounded-lg mt-5 hover:bg-zinc-950 hover:border-zinc-953 hover:text-zinc-100 hover:cursor-pointer hover:duration-700"
@@ -51,11 +67,19 @@ const Signup = () => {
               />
             </div>
             <div className="mt-5 flex justify-between items-center">
-              <h1 className="underline text-lg font-extrabold text-zinc-950 uppercase tracking-widest">Old to Bistro Boss?</h1>
-              <Link to="/login"><button className="duration-700 text-xl px-6 py-1 font-extrabold bg-zinc-950 text-orange-500 border-2 border-zinc-950 rounded-lg uppercase hover:border-zinc-100 hover:bg-zinc-100 hover:cursor-pointer hover:duration-700">Login</button></Link>
+              <h1 className="underline text-lg font-extrabold text-zinc-950 uppercase tracking-widest">
+                Old to Bistro Boss?
+              </h1>
+              <Link to="/login">
+                <button className="duration-700 text-xl px-6 py-1 font-extrabold bg-zinc-950 text-orange-500 border-2 border-zinc-950 rounded-lg uppercase hover:border-zinc-100 hover:bg-zinc-100 hover:cursor-pointer hover:duration-700">
+                  Login
+                </button>
+              </Link>
             </div>
             <div className="mt-4 flex justify-center items-center">
-            <button className="duration-700 text-xl px-6 py-1 font-extrabold bg-zinc-950 text-orange-500 border-2 border-zinc-950 rounded-lg uppercase hover:border-zinc-100 hover:bg-zinc-100 hover:cursor-pointer hover:duration-700">Sign Up With Google</button>
+              <button className="duration-700 text-xl px-6 py-1 font-extrabold bg-zinc-950 text-orange-500 border-2 border-zinc-950 rounded-lg uppercase hover:border-zinc-100 hover:bg-zinc-100 hover:cursor-pointer hover:duration-700">
+                Sign Up With Google
+              </button>
             </div>
           </form>
         </div>
@@ -66,14 +90,15 @@ const Signup = () => {
             <p className="text-5xl font-normal mt-2">
               Look, Who Just Showed Up !!!
             </p>
-            <p className="text-2xl font-bold mt-10 uppercase tracking-widest text-yellow-500">{`
+            <p className="text-2xl font-bold mt-10 uppercase tracking-widest text-yellow-500">
+              {`
               Aren't You Hungry Yet? I Bet You Are !!! `}
             </p>
           </div>
         </div>
       </div>
     </div>
-   );
+  );
 };
 
 export default Signup;
